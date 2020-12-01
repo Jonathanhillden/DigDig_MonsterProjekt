e80b9e91be84a53c19857f24d44728c4daf2c34d
Pokenemo8 <otto.pettersson@elev.ga.lbs.se>
2020-12-01 15:54:54 +0100

    Basic healthbar

---
+++ DigDig_Monster_Projekt/Assets/Scripts/Healthbar.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Healthbar : MonoBehaviour
+{
+    public Slider slider;
+
+    public int maxhealth;
+    public int health;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = maxhealth;
+        slider.value = maxhealth;
+    }
+
+    public void IncreaseHealth(int health)
+    {
+        health++;
+        UpdateHealth(health);
+    }
+
+    public void DecreaseHealth(int health)
+    {
+        health--;
+        UpdateHealth(health);
+    }
+
+    public void UpdateHealth(int health)
+    {
+        slider.value = health;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+}
